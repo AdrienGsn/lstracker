1fa2265507990228fa8c155c78250e78bf8b1994
AdrienGsn <06.volts-papier@icloud.com>
2025-10-17 23:28:45 +0200

    preview

--- src/actions/fantome/index.ts
+++ src/actions/fantome/index.ts
@@ -1,6 +1,5 @@
 "use server";
 
-import { logger } from "@/lib/logger";
 import { prisma } from "@/lib/prisma";
 import { action } from "@/lib/safe-action";
 import { FamtomeSchema } from "./schema";
@@ -8,11 +7,10 @@ import { FamtomeSchema } from "./schema";
 export const fantomeAction = action
 	.inputSchema(FamtomeSchema)
 	.action(async ({ parsedInput: { lat, lng } }) => {
-		logger.debug(lat, lng);
 		await prisma.marker.create({
 			data: {
-				lat: Number(lat),
-				lng: Number(lng),
+				lat: Number(lat.replace(",", ".")),
+				lng: Number(lng.replace(",", ".")),
 				label: "Fantome",
 			},
 		});
--- src/actions/fantome/schema.ts
+++ src/actions/fantome/schema.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 export const FamtomeSchema = z.object({
-	lat: z.string().regex(/^-?\d+(\.\d+)?$/, "Nombre invalide"),
-	lng: z.string().regex(/^-?\d+(\.\d+)?$/, "Nombre invalide"),
+	lat: z.string().regex(/^-?\d+([.,]\d+)?$/, "Nombre invalide"),
+	lng: z.string().regex(/^-?\d+([.,]\d+)?$/, "Nombre invalide"),
 });
 
 export type FantomeSchemaType = z.infer<typeof FamtomeSchema>;
