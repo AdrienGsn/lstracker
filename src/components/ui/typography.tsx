f3b595d39461d930df47ac020a121449d9559f80
AdrienGsn <06.volts-papier@icloud.com>
2025-11-20 10:25:31 +0100

    refactor: update typography component styles and default variants for improved consistency and clarity

--- src/components/ui/typography.tsx
+++ src/components/ui/typography.tsx
@@ -1,4 +1,6 @@
-import { cva, type VariantProps } from "class-variance-authority";
+import type { VariantProps } from "class-variance-authority";
+import { cva } from "class-variance-authority";
+import type React from "react";
 import type {
 	ComponentPropsWithoutRef,
 	ElementType,
@@ -21,27 +23,27 @@ type PolymorphicProps<E extends ElementType> = PropsWithChildren<
 const typographyVariants = cva("", {
 	variants: {
 		variant: {
-			h1: "scroll-m-20 text-4xl font-extrabold tracking-tight text-foreground lg:text-5xl",
-			h2: "scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight text-foreground transition-colors first:mt-0",
-			h3: "scroll-m-20 text-xl font-semibold tracking-tight text-foreground",
-			p: "leading-7 text-foreground [&:not(:first-child)]:mt-6",
-			base: "text-foreground",
-			quote: "mt-6 border-l-2 pl-6 italic text-foreground",
-			code: "relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold text-foreground",
+			h1: "scroll-m-20 font-caption text-4xl font-extrabold tracking-tight lg:text-5xl",
+			h2: "scroll-m-20 text-xl font-semibold tracking-tight transition-colors md:text-2xl lg:text-3xl",
+			h3: "scroll-m-20 text-xl font-semibold tracking-tight",
+			p: "text-lg leading-7 not-first:mt-6",
+			base: "text-lg",
+			quote: "mt-6 border-l-2 pl-6 italic",
+			code: "relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold",
 			lead: "text-xl text-muted-foreground",
-			large: "text-lg font-semibold text-foreground",
-			small: "text-sm font-medium leading-none text-foreground",
+			large: "font-semibold md:text-lg",
+			small: "text-sm font-medium leading-none",
 			muted: "text-sm text-muted-foreground",
-			link: "font-medium text-foreground hover:underline",
+			link: "font-medium text-cyan-600 hover:underline dark:text-primary",
 		},
 	},
 	defaultVariants: {
-		variant: "base",
+		variant: "p",
 	},
 });
 type TypographyCvaProps = VariantProps<typeof typographyVariants>;
 
-const defaultElement = "p";
+const defaultElement = "base";
 
 const defaultElementMapping: Record<
 	NonNullable<TypographyCvaProps["variant"]>,
@@ -75,6 +77,8 @@ export function Typography<E extends ElementType = typeof defaultElement>({
 		<Component
 			{...(restProps as ComponentPropsWithoutRef<E>)}
 			className={cn(typographyVariants({ variant }), className)}
+			data-typography
+			data-typography-variant={variant}
 		>
 			{children}
 		</Component>
