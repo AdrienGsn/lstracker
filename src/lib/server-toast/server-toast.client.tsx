f46c067e507d5d8f894f06d2c44e646195113632
AdrienGsn <06.volts-papier@icloud.com>
2025-11-20 10:03:26 +0100

    feat: implement server-side toast management with client integration for displaying notifications

---
+++ src/lib/server-toast/server-toast.client.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { startTransition, useEffect, useOptimistic, useState } from "react";
+import { toast as sonnerToast } from "sonner";
+
+import type { ServerToastType } from "./server-toast.type";
+
+type Toast = {
+	id: string;
+} & ServerToastType;
+
+export function ClientToasts({
+	toasts,
+	dismissToast,
+}: {
+	toasts: Toast[];
+	dismissToast: (id: string) => Promise<void>;
+}) {
+	const [optimisticToasts, remove] = useOptimistic(toasts, (current, id) =>
+		current.filter((toast) => toast.id !== id)
+	);
+
+	const localToasts = optimisticToasts.map((toast) => ({
+		...toast,
+		dismiss: async () => {
+			remove(toast.id);
+
+			await dismissToast(toast.id);
+		},
+	}));
+
+	const [sentToSonner, setSentToSonner] = useState<string[]>([]);
+
+	useEffect(() => {
+		localToasts
+			.filter((toast) => !sentToSonner.includes(toast.id))
+			.forEach((toast) => {
+				setSentToSonner((prev) => [...prev, toast.id]);
+				sonnerToast(toast.message, {
+					id: toast.id,
+					// @ts-expect-error - Sonner API currently HAVE a `type` property but it's not typed.
+					type: toast.type,
+					onDismiss: () => startTransition(toast.dismiss),
+					onAutoClose: () => startTransition(toast.dismiss),
+				});
+			});
+	}, [localToasts, sentToSonner]);
+
+	return null;
+}
---
+++ src/lib/server-toast/server-toast.server.tsx
@@ -0,0 +1,41 @@
+import { cookies } from "next/headers";
+
+import { ClientToasts } from "./server-toast.client";
+import { ServerToastSchema } from "./server-toast.type";
+
+async function dismissToast(id: string) {
+	"use server";
+
+	const cookieStore = await cookies();
+
+	cookieStore.delete(id);
+}
+
+export async function ServerToaster() {
+	const cookieStore = await cookies();
+
+	const toasts = cookieStore
+		.getAll()
+		.filter((cookie) => cookie.name.startsWith("toast-") && cookie.value)
+		.map((cookie) => {
+			try {
+				const parsed = ServerToastSchema.safeParse(
+					JSON.parse(cookie.value)
+				);
+
+				if (!parsed.success) {
+					return undefined;
+				}
+
+				return {
+					id: cookie.name,
+					...parsed.data,
+				};
+			} catch {
+				return undefined;
+			}
+		})
+		.filter((toast) => toast !== undefined);
+
+	return <ClientToasts toasts={toasts} dismissToast={dismissToast} />;
+}
